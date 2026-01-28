8bd7ea2e0d92c9c133e26428998d4cbc2f2365f3
will54-dev <will.haggard@ucas-edn.net>
2026-01-28 10:34:58 -0700

    finised my code

--- RPG Character Manager/code_rpg_character_manager.py
+++ RPG Character Manager/code_rpg_character_manager.py
@@ -1,26 +1,20 @@
 # WH, SH, JQ 2nd
 
 from will_code_rpg import character_creator
-class itme:
-    weight = int()
-    value = str()
-    description = str()
 
 characters = dict()
 races = {
 
-
 }
 classes = {
-
 
 }
 
 def main_menu():
     while True:
-        match input("Do you ant to (as a number).\n1: Make a character \n2: Edit a character \n3: Compare a character \n4: Exit").strip():
+        match input("Do you ant to (as a number).\n1: Make a character \n2: Edit a character \n3: Compare a character \n4: Exit\n").strip():
             case "1":
-                character_creator()
+                character_creator(races,classes,characters)
             case "2":
                 pass
             case "3":
@@ -29,3 +23,5 @@ def main_menu():
                 pass
             case _:
                 pass
+
+main_menu()
--- RPG Character Manager/will_code_rpg.py
+++ RPG Character Manager/will_code_rpg.py
@@ -1,11 +1,5 @@
 # WH 2nd 
 
-characters = dict()
-modifer_list = {
-    "human": (1,2,3,4,5,6,7)
-
-}
-
 def help_isint_input(text):
     while True:
         want = input(text)
@@ -18,29 +12,59 @@ def help_isint_input(text):
                 print("Not a number.")
 
 def modifier_selector(text, modifer_list):
-    print(f"what {text} do you want?")
-    count = 0
-    list_to_modifer = list()
-    for class_race in modifer_list.keys():
-        list_to_modifer.append(class_race)
-        count += 1
-        print(f"{count}: {class_race}")
-    want = input()
+    while True:
 
-    try:
-        class_race = modifer_list[want.strip()]
-        class_race = want
-    except:
-        try:
-            class_race = modifer_list[]
+        while True:
+            print(f"what {text} do you want?")
+            count = 0
+            list_to_modifer = list()
+            for class_race in modifer_list.keys():
+                list_to_modifer.append(class_race)
+                count += 1
+                print(f"{count}: {class_race}")
+            want = input()
 
-def character_creator(characters = dict):
+            try:
+                class_race = modifer_list[want.strip()]
+                class_race = want
+                break
+            except:
+                try:
+                    want = int(want)
+                    if want > 0:
+                        class_race = modifer_list[list_to_modifer[want-1]]
+                        class_race = list_to_modifer[int(want)-1]
+                        break
+                    else:
+                        print("Not an option.")
+                except:
+                    print("Not an option.")
+        stats = modifer_list[class_race]
+        stats_text = ["strength","dexterity","wisdom","charisma","intelligence","constitution"]
+        print("Do you want:")
+        for item in range(6):
+            c_stat = stats[item]
+            c_text = stats_text[item]
+            if c_stat > 0:
+                print(f"+{c_stat}: {c_text}")
+            else:
+                print(f"{c_stat}: {c_text}")
+        want = input("(1: yes/ 2: no)\n").strip()
+        while True:
+            if want == "1" or want == "yes":
+                return class_race
+            elif want == "1" or want == "no":
+                break
+            else:
+                print("not an option.")
+
+def character_creator(races, classes ,characters = dict):
     character_name = input("What is the new character's name?\n")
     new_character = dict()
+    new_character["race"] = modifier_selector("race",races)
+    new_character["class"] = modifier_selector("class",classes)
     text = ["level","strength","dexterity","wisdom","charisma","intelligence","constitution"]
     for item in text:
         new_character[item] = help_isint_input(f"What is {character_name}'s {item}?\n")
     characters[character_name] = new_character
     return characters
-
-modifier_selector("race",modifer_list)
